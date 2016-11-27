f3629a8d057907be97c726b9a85986f9b6234f12
surwill <wjsurmak@uwaterloo.ca>
2016-11-27 12:28:03 -0500

    added level changing

--- LevelController.h
+++ LevelController.h
@@ -2,12 +2,12 @@
 #include "level.h"
 
 class LevelController{
-  Level * currentLevel;
+  private:
+    Level * currentLevel;
 
   public:
     LevelController(int level);
     void changeLevel(int levelNum);
     void setFileName(std::string fileName);
-    const Level * getLevel();
-}
-
+    const Level * const getLevel(); //2 consts: Won't modify where it's pointing and what it's pointing at
+};
