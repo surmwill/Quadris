84bf5011357cdaa82ca4fc0d8169ece0c98c416d
surwill <wjsurmak@uwaterloo.ca>
2016-11-26 22:30:33 -0500

    changed file name

---
+++ LevelController.cc
@@ -0,0 +1,22 @@
+#include <string>
+#include "levelController.h"
+
+using namespace std;
+
+LevelController::LevelController(int level){
+
+}
+
+void LevelController::changeLevel(int levelNum){
+
+}
+
+void LevelController::setFileName(std::string fileName){
+
+}
+
+const Level * LevelController::getLevel(){
+  return currentLevel;
+}
+
+
