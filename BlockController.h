9dd7bae86dda91ca95f4a827a703f60bfccf52d7
surwill <wjsurmak@uwaterloo.ca>
2016-11-26 22:49:43 -0500

    fixed spacing and semicolon

--- BlockController.h
+++ BlockController.h
@@ -8,24 +8,25 @@ class Grid;
 class Block;
 
 class BlockController {
-  public:
-    BlockController(Level *level, Grid *grid)
-    void left();
-    void right();
-    void down();
-    void rotatecc();
-    void rotatecw();
-    void drop();
-    void genBlock();
+ public:
+   BlockController(Level *level, Grid *grid)
+   void left();
+   void right();
+   void down();
+   void rotatecc();
+   void rotatecw();
+   void drop();
+   void genBlock();
 
-  private:
-    void attachCurrBlockToGrid();
+ private:
+   void attachCurrBlockToGrid();
 
-    std::unique_ptr <Grid> grid; //Block Controller is reponsible for deleting grid
-    std::unique_ptr <Block> currBlock; //Block Controller is responsible for deleting currBlock
-    std::unique_ptr <Block> nextBlock; //Block Controller is responsible for deleing nextBlock
-    Level * level;
-    std::vector <Cell*> generatingArea;
+   std::unique_ptr <Grid> grid; //Block Controller is reponsible for deleting grid
+   std::unique_ptr <Block> currBlock; //Block Controller is responsible for deleting currBlock
+   std::unique_ptr <Block> nextBlock; //Block Controller is responsible for deleing nextBlock
+   Level * level;
+   std::vector <Cell*> generatingArea;
+};
 
 #ifndef
 
