9f6a054191852e3b3a966fb399d42e33cff7d4ed
surwill <wjsurmak@uwaterloo.ca>
2016-11-29 01:04:38 -0500

    worked on TextDisplay

--- TextDisplay.cc
+++ TextDisplay.cc
@@ -8,18 +8,45 @@ using namespace std;
 
 TextDisplay::TextDisplay(int rows, int cols) {
   for(int i = 0; i < rows; i++) {
-    for(int i = 0; i < cols; i++) {
+    for(int j = 0; j < cols; i++) {
       textGrid[i].emplace_back(' ');
     }
   }
 }
 
+//notifications of a cells states
 void TextDisplay::notify(Subject &whoNotified) {
 }
 
+//displays the board in text
 void TextDisplay::display(const Score &score) {
+  int spacing = 4;
+  int dashLength = 15;
+
+  //Top of board
+  cout << "Level: ";
+  cout << setw(spacing);
+  cout << score->getLevel(); << endl;
+  cout << "Score: ";
+  cout << setw(spacing);
+  cout << score.getCurrScore();
+  cout << "Hi Score: ";
+  cout << setw(spacing);
+  cout << score.getHighScore();
+  for(int i = 0; i < dashLength; i++) {
+    cout << " " << endl;
+  }
+
+  for(int i = 0; i < rows; i++) {
+    for(int j = 0; j < cols j++) {
+      cout << textGrod[i];
+    }
+    cout << "\n";
+   }
+  }
 }
 
+//displays the board with a hint
 void TextDisplay::display(const Score &score, const vector <vector <int>> &coords) {
 }
 
