8ddb1f9f4ed867dc9741fb0510567c04f922222c
surwill <wjsurmak@uwaterloo.ca>
2016-11-30 23:42:57 -0500

    added some graphics functionality

--- GraphicsDisplay.cc
+++ GraphicsDisplay.cc
@@ -2,15 +2,28 @@
 #include "GraphicsDisplay.h"
 #include "Score.h"
 #include "View.h"
+#include "Subkect.h"
+#include <map>
 #include <vector>
-#include "Subject.h"
 
 using namespace std;
 
-GraphicsDisplay::GraphicsDisplay(int rows, int cols): rows{rows}, cols{cols}, cellWidth{40},
-  cellHeight{40}, topSpace{60} {}
+GraphicsDisplay::GraphicsDisplay(int rows, int cols): rows{rows}, cols{cols}, totalLength{cols * cellWidth}, 
+  totalHeight{(rows * cellHeight) + topSpace}, Xwindow{totalLength, totalHeight} {
+  symToColour.emplace_back("i", Xwindow::Magenta); 
+  symToColour.emplace_back("j", Xwindow::Red);
+  symToColour.emplace_back("l", Xwindow::Green);
+  symToColour.emplace_back("o", Xwindow::Cyan);
+  symToColour.emplace_back("s", Xwindow::Brown);
+  symToColour.emplace_back("z", Xwindow::Orange);
+  symToColour.emaplce_back("t", Xwindow::Blue);
+}
 
 void GraphicsDisplay::notify(const Subject &whoNotified) {
+  int row = whoNotified.getCoords()[0];
+  int col = whoNotified.getCoords()[1];
+
+  fillCell(row, col, symbolToColour(Subject.getSymbol());
 }
 
 void GraphicsDisplay::display(const Score &score) {
@@ -21,3 +34,23 @@ void GraphicsDisplay::display(const Score &score, const vector <vector <int>> &c
 
 void GraphicsDisplay::clear() {
 } 
+
+Xwindow GraphicsDisplay::symbolToColour(char symbol) {
+  symToColour.find(symbol) == symToColour.end()? return Xwindow::Black : return symToColour->second;
+}
+
+//Each Cell has a white border outline and a filled coloured center
+void GraphicsDisplay::fillCell(int row, int col, Xwindow colour) {
+  int indent = 3; //how thick the white border between cells are
+
+  int coordX = col * cellWidth + indent; //indent to allow white outline
+  int coordY = row * cellHeight + indent + topSpace; //indent to allow white outline
+
+  int fillWidth = cellWidth - (indent * 2); //only fills the center of the cell with colour
+  int fillHeight = cellHeight - (indent * 2); //only fills the center of the cell with colour
+
+  win.fillRectangle(coordX, coordY, fillWidth, fillHeight, colour); //draws the rectangle
+}
+
+void GraphicsDisplay::drawTop(int score, int highScore, int level) {
+}
--- GraphicsDisplay.h
+++ GraphicsDisplay.h
@@ -2,6 +2,7 @@
 #define GRAPHICS_H 1
 
 #include "View.h"
+#include "Window.h"
 #include <vector>
 #include <map>
 
@@ -13,21 +14,23 @@ class GraphicsDisplay: public View {
   GraphicsDisplay(int rows = 18, int cols = 11);
   void notify(const Subject &whoNotified) override; //Cell's notify the board whenever they change state, so we can update the cells accordingly
   void display(const Score &score) override; //displays the board, calling both drawTop() and drawMiddle()
-  void display(const Score &score, std::vector <const std::vector <int>> & coords) override; //displays the hint, only calling drawMiddle()
+  void display(const Score &score, const std::vector <const std::vector <int>> & coords) override; //displays the hint, only calling drawMiddle()
   void clear() override; //clears the board and redraws the curr score
 
  private:
    int rows; //rows of cells we want
    int cols; //cols of cells we want
-   int cellWidth; //the width in pixels of a cell in the display
-   int cellHeight; //the height in pixels of a cell in the display
-   int topSpace; //space reserved for the drawTop() function
+   int cellWidth = 40; //the width in pixels of a cell in the display
+   int cellHeight = 40; //the height in pixels of a cell in the display
+   int topSpace = 60; //space reserved for the drawTop() function
    int totalLength; //the total length in pixels of the window
    int totalHeight; //the total height in pixels of the window
-
-   Xwindow symbolToColour(char symbol); //returns a colour based on symbol
-   void drawTop(); //displays, score, level, and next block
-   void drawMiddle(); //displays the grid
+   Xwindow win; //the Xwindow object that allows us to display amd update graphics
+   std::map <char, Xwindow> symToColour; //maps a char symbol key to an Xwindow::Color
+   
+   Xwindow symbolToColour(char symbol); //Accesses the map with additional error checking
+   void drawTop(int score, int highScore, int level); //displays, score, level, and next block
+   void fillCell(int row, int col, Xwindow colour); //fills the cell at (row,col) with colour
 };
 
 #endif
