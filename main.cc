2fb03a737a528fbfd8b9e0ed919d32c621d71799
surwill <wjsurmak@uwaterloo.ca>
2016-12-02 00:05:46 -0500

    fuck

--- main.cc
+++ main.cc
@@ -1,4 +1,12 @@
-#include <iostream>
+#include "CmdInterpreter.h"
 
-int main() {
+void parseArgument(const char *argument) {
+  switch(argument) 
+}
+
+int main(int argc, char *argv[]) {
+  if(argc > 1) {
+    for(int i = 1; i < argv; i++) {
+       parseArgument(argv[i])
+    }
 }
