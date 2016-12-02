318d2a23c733ae054b96022840eb613256881ddf
surwill <wjsurmak@uwaterloo.ca>
2016-12-02 00:08:47 -0500

    fuck

--- main.cc
+++ main.cc
@@ -1,12 +1,15 @@
 #include "CmdInterpreter.h"
+#include <string>
 
-void parseArgument(const char *argument) {
-  switch(argument) 
+void parseArgument(std::string arg) {
+  switch(arg)  {
+    case : 
 }
 
 int main(int argc, char *argv[]) {
   if(argc > 1) {
     for(int i = 1; i < argv; i++) {
-       parseArgument(argv[i])
+       std::string arg{argv[i]};
+       parseArgument(arg)
     }
 }
