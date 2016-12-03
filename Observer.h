d88d7763e803a39dd22f89543125ec85fb77c041
surwill <wjsurmak@uwaterloo.ca>
2016-12-02 21:06:04 -0500

    cleaned

--- Observer.h
+++ Observer.h
@@ -1,14 +1,16 @@
 #ifndef _OBSERVER_H_
 #define _OBSERVER_H_
+
 #include "Subscriptions.h"
+
 class Subject;
 class Cell;
 
 class Observer {
  public:
-  virtual void notify(Subject &whoNotified) = 0;  // pass the Cell that called
+  virtual void notify(const Subject &whoNotified) = 0;  // pass the Cell that called
                                                   // the notify method
-  virtual SubscriptionType subType() const = 0;
+  virtual SubscriptionType subType() const = 0; //The subscription type of an observer
   virtual ~Observer() = default;
 };
 #endif
