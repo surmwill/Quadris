18570c109a05d9b2081ccab37de86ccfbef74862
jeffDuce <jmduce@uwaterloo.ca>
2016-12-03 02:50:20 -0500

    fixed and cleaned up rotation code

--- Block.cc
+++ Block.cc
@@ -23,34 +23,46 @@ virtual void Block::left() {
 virtual void Block::right() {
 }
 
-virtual void Block::rotatecw() {
-  vector<Cell*> newBlock;
-  int cellNum = 0;
-
+virtual void Block::rotate(bool cc) {
+  // make a rotated block
+  vector<Cell> rotatedBlock;
   for (int i = 0; i < 3; i++){
     for (int j = 0; j < 3; j++){
-      if (n <= 15){
-        blockCells = newBlock;
-        return;
+      // create new cell
+      Cell newCell;
+
+      // Find the index of the Cell who will replace the current cell
+      int oldLocation = ((13+i) - (4*j)) - 1;
+      if (cc){
+        oldLocation = (15 - oldLocation);
       }
-      newBlocks.push_back(blockCells[((13 + i) - (4 * j))]);
+
+      // make sure that the rotate is valid
+      if (!blockCells[oldLocation].filled() || memberCell(oldLocation)){
+        // if the destination of the cell at oldLocation is blocked, cancel rotation
+        if (blockCells[((i*4) + j)].filled() && !memberCell((i*4)+j)){
+          return;
+        }
+
+        // set new cell content
+        Cell.setContent(blockCells[oldLocation]);
+      }
     }
+  }
+
+  // set the block cells to their rotated configuration
+  for (int i = 0; i < 15; i++){
+    blockCells[i]->setContent(&rotatedBlock[i]);
   }
 }
 
-virtual void Block::rotatecc() {
-  vector<Cell*> newBlock;
-  int cellNum = 0;
-
-  for (int i = 0; i < 3; i++){
-    for (int j = 0; j < 3; j++){
-      if (n <= 15){
-        blockCells = newBlock;
-        return;
-      }
-      newBlocks.push_back(blockCells[((4 * j) + (4 - i))]);
+bool memberCell(int index){
+  for (int i: filledIndices){
+    if (index == i){
+      return true;
     }
   }
+  return false;
 }
 
 virtual void Block::down() {
--- Block.h
+++ Block.h
@@ -10,17 +10,18 @@ class Block {
   Block(std::vector <char> blockDesign, int levelGenerated, int blockSize = 4);
   virtual void left();
   virtual void right();
-  virtual void rotatecw();
-  virtual void rotatecc();
-  virtual bool autoDrop(); 
+  virtual void rotate(bool cc);
+  virtual bool autoDrop();
   virtual bool drop():
   virtual bool down();
 
  protected:
   std::vector <Cell*> *getBlockCells();
- 
+  vector<int> filledIndices;
+
  private:
   std::vector <Cell*> blockCells;
+  bool memberCell(int index);
 };
 
 #endif 
