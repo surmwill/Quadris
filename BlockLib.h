04f6a5bf2be70cec443186035125c742037aa49b
surwill <wjsurmak@uwaterloo.ca>
2016-12-04 01:37:25 -0500

    BlockLib compiles

--- BlockLib.h
+++ BlockLib.h
@@ -18,7 +18,7 @@ class BlockLib {
    std::vector <std::vector <char>> zBlock() const; //returns a 2D vector of 4x4 chars, that represents the design of an Z block
    std::vector <std::vector <char>> tBlock() const; //returns a 2D vector of 4x4 chars, that represents the design of an T block
 
-   typedef std::vector <std::vector <char>> (*BlockLayoutFn)(char); //Function pointer with return type <vector <vector <char>>, paramter char, and generalized name of BlockLayoutFn
+   typedef std::vector <std::vector <char>> (BlockLib::*BlockLayoutFn)(void) const; //Function pointer with return type <vector <vector <char>>, paramter char, and generalized name of BlockLayoutFn
    std::map <char, BlockLayoutFn> layoutMap; //Maps a function that returns a block design to a type of block
 };
 
