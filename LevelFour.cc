97afec632612178c98505919f83e61462fec8047
jeffDuce <jmduce@uwaterloo.ca>
2016-12-04 16:01:48 -0500

    LevelFour now compiles

--- LevelFour.cc
+++ LevelFour.cc
@@ -1,14 +1,51 @@
 #include "LevelFour.h"
 #include "Level.h"
+#include "HeavyBlock.h"
+#include "DirectedBlock.h"
 #include <vector> 
 #include <string> 
-#include "Block.h"
 
 using namespace std;
 
 Block* LevelFour::genBlock() {
-}
+  // create a model for the probability breakdown
+  vector<char> symbolProbability;
+
+  // I block probability (1/9)
+  symbolProbability.push_back('I');
+
+  // J block probability (1/9)
+  symbolProbability.push_back('J');
+
+  // L block probability (1/9)
+  symbolProbability.push_back('L');
+
+  // O block probability (1/9)
+  symbolProbability.push_back('O');
+
+  // S block probability (2/9)
+  symbolProbability.push_back('S');
+  symbolProbability.push_back('S');
+
+  // Z block probability (2/9)
+  symbolProbability.push_back('Z');
+  symbolProbability.push_back('Z');
+
+  // O block probability (1/9)
+  symbolProbability.push_back('O');
+
+  int randInt = rand() % 9;
+
+  return genBlock(symbolProbability[randInt]);
+ }
 
 Block* LevelFour::genBlock(char type) {
+  if ((blockNum % 5) == 0){
+    DirectedBlock *dBlock = new DirectedBlock((getBlockLib()->getFlattenedBlockLayout(type)), 4);
+    return dBlock;
+  }
+
+  HeavyBlock *hBlock = new HeavyBlock((getBlockLib()->getFlattenedBlockLayout(type)), 4);
+  return hBlock;
 }
 
