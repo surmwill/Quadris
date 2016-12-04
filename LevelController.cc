5a70e391bb02da334ec884320f4c4f418fa843e7
jeffDuce <jmduce@uwaterloo.ca>
2016-12-04 03:09:27 -0500

    added Level

---
+++ LevelController.cc
@@ -0,0 +1,33 @@
+#include <string>
+#include "LevelController.h"
+
+using namespace std;
+
+LevelController::LevelController(int level) {
+  changeLevel(level);  
+}
+
+void LevelController::changeLevel(int levelNum){
+  switch(levelNum) {
+    case 0: currentLevel = new LevelZero{"sequence.txt"};
+      break;
+    case 1: currentLevel = new LevelOne{};
+      break;
+    case 2: currentLevel = new LevelTwo{};
+      break;
+    case 3: currentLevel = new LevelThree{};
+      break;
+    case 4: currentLEvel = new LevelFour{};
+      break;
+  }
+}
+
+void LevelController::setFileName(std::string fileName){
+  currentLevel->setFileName(fileName);
+}
+
+const Level * LevelController::getLevel(){
+  return currentLevel;
+}
+
+
