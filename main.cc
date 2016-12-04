766248cfb90a7ea83a520305bbb9f389a3874cdf
surwill <wjsurmak@uwaterloo.ca>
2016-12-04 17:52:17 -0500

    added debug

--- main.cc
+++ main.cc
@@ -1,10 +1,14 @@
 #include "cmdInterpreter.h"
 #include <iostream>
 
+#define DEBUG 1
+
 int main(int argc, char *argv[]) {
   //starts the game
   std::istream *in = &std::cin; //can be changed to istream *in = new ifstream(fname.c_str()) where fname is a user inputted string of an input source
   CmdInterpreter cmd{in, argc, argv};
+
+  if(DEBUG == 1) std::cout << "cmdInterpreter constructed" << std::endl;
   cmd.startGame();
 }
 
