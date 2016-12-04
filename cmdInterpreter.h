7c31f3daa9124a40f8ec81d0075edbb6ac118e55
surwill <wjsurmak@uwaterloo.ca>
2016-12-04 02:57:10 -0500

    did shit

--- cmdInterpreter.h
+++ cmdInterpreter.h
@@ -1,14 +1,18 @@
 #include <string>
-#include "quatris.h"
+#include "Quadris.h"
 
 class CmdInterpreter {
-  Quatris mController;
-  istream stream;
+  private:
+    Quadris mController; //Calls different parts of the program
+    istream *stream; //The input stream
 
-  void interpretCommand(std::string cmd);
-  
+    void interpretCommand(std::string cmd); //Determines what part of the program to call with a given command
+    void parseArguments(std::string arg1, std::string arg2); //Parses command line arguments
+    bool goodFile(std::string s); //checks if a file with name s can be opened
+    bool isNumber(std::string s); //checks if string s is a valid number
+
   public:
-    CmdInterpreter(std::istream * in);
-    void startGame();
+    CmdInterpreter(std::istream * in); //Triggers the cosntruction, of the rest of the program, sets the input stream, parses command line arguments
+    void startGame(); //Begins the input and evaluation of commands
 }
 
--- main.cc
+++ main.cc
@@ -1,67 +1,11 @@
 #include "CmdInterpreter.h"
-#include <string>
 #include <iostream>
-#include <locale> //needed for isdigit()
-#include <cstdlib> //needed for rand()
-#include <ctime> //need for time(0)
-#include <fstream>
-
-//checks if string s is a valid number
-bool isNumber(std::string s) {
-  if(s.length() < 1) return false;
-
-  for(auto &n: s) {
-    if(!isdigit(n)) return false;
-  }
-  return true;
-}
-
-//checks if file with filename can be opened
-bool goodFile(std::string filename) {
-  if(filename.length() < 1) retrurn false;
-
-  std::ofstream myfile{filename};
-  myfile.open()? return true : return false;
-}
-
-//parses the command line arguments
-void parseArgument(std::string arg1, std::string arg2) {
-  if(arg1 == "-text") cmd.textOnly();
-  else if(arg1 == "-seed") {
-    if(isNumber(arg2) cmd.setSeed(std::stoi(arg2)) //check that we have a valid second argument
-    else {
-      //otherwise randomly give them a seed
-      std::cerr << "not given a number for a seed, generating a seed for you" << std::endl;
-      std::srand(std::time(0));
-      int rand = std::rand() % 100000;
-      cmd.setSeed(std::stoi(rand));
-    }
-  }
-  else if(arg1 == "-scriptfile") {
-    if(goodFile(arg2)) cmd.setSequence(arg2); //check that we have a valid second argument
-    else std::cerr << "could not read scriptfile" << std::endl; 
-  }
-  else if(arg1 == "-startlevel") {
-   if(isNumber(arg2)) cmd.setLevel(std::stoi(arg2)); //check that we have a valid second argument
-   else std::cerr << "not given a number for level, starting at level 0" << std::endl;
-  }  
-}
 
 int main(int argc, char *argv[]) {
+  //starts the game
   istream *in == &std::cin; //can be changed to istream *in = new ifstream(fname.c_str()) where fname is a user inputted string of an input source
   CmdInterpreter cmd{in};
-
-  //parse the command line arguments
-  if(argc > 1) {
-    for(int i = 1; i < argv; i++) {
-       std::string arg1{argv[i]}; //store the first argument in a string
-       i < (argv - 1)? std::string arg2{argv[i + 1]} : std::string arg2{""}; //if there is no second argument, set the second argument to "" to trigger invalid input
-       parseArgument(arg1, arg2); //parse the arguments
-    }
-  }
-
-  //starts the game
-  cmd.startGame();
+  cmd.startGame(argc, argv);
 }
 
 
