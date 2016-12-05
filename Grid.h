3b4575240f6bba9583034cee2047db35c7b8134c
jeffDuce <jmduce@uwaterloo.ca>
2016-12-05 13:00:22 -0500

    added score as an observer to cells

--- Grid.h
+++ Grid.h
@@ -1,5 +1,6 @@
 #include "Cell.h"
 #include "View.h"
+#include "Observer.h"
 #include <vector>
 
 class Grid{
@@ -7,6 +8,6 @@ class Grid{
   std::vector<std::vector<Cell>> grid;
 
  public:
-  Grid(const std::vector <View*> & views, int row = 18, int col = 11);
+  Grid(const std::vector <View*> & views, Observer * score, int row = 18, int col = 11);
   Cell * getCell(int row, int col);
 };
